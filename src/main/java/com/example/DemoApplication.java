36a7fdb131b5aabb4fadacd6c9cbb84d96631bbd
trainer4 <trainer4@trainer4s-MacBook-Pro.local>
2017-04-19 20:56:17 -0600

    Unit8Ex2

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,7 @@
 package com.example;
 
-import com.example.entity.Employee;
-import com.example.repositories.EmployeeRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DemoApplication {
